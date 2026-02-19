f6757f6c97decbd7ed2c68a61ccb37a3cf97db72
federico <fedequinti98@gmail.com>
2026-02-19 19:20:29 +0100

    Working on Dropdown Menu style

--- components/UserDropdown.tsx
+++ components/UserDropdown.tsx
@@ -24,28 +24,40 @@ const UserDropdown = () => {
         <DropdownMenu>
             <DropdownMenuTrigger asChild>
                 <Button variant="ghost" className="flex items-centergap-3 text-gray-4 hover:text-yellow-500" >
+                    <div className="hidden md:flex flex-col items-start">
+                        <span className="text-base font-medium text-gray-400">
+                            {user.name}
+                        </span>
+                    </div>
                     <Avatar className="h-8 w-8">
                         <AvatarImage src="https://github.com/shadcn.png" />
                         <AvatarFallback className="bg-yellow-500 text-yellow-900 text-sm font-bold">
                             {user.name[0]}
                         </AvatarFallback>
                     </Avatar>
-                    <div className="hidden md:flex flex-col items-start">
-                        <span className="text-base font-medium text-gray-400">
-                            {user.name}
-                        </span>
-                    </div>
+
                 </Button>
             </DropdownMenuTrigger>
-            <DropdownMenuContent>
-                <DropdownMenuLabel>My Account</DropdownMenuLabel>
-                <DropdownMenuSeparator />
-                <DropdownMenuItem>Profile</DropdownMenuItem>
-                <DropdownMenuItem>Billing</DropdownMenuItem>
-                <DropdownMenuItem>Team</DropdownMenuItem>
-                <DropdownMenuItem>Subscription</DropdownMenuItem>
+            <DropdownMenuContent className="text-gray-400">
+                <DropdownMenuLabel>
+                    <div className="flex relative items-center gap-3 py-2">
+                        <Avatar className="h-10 w-10">
+                            <AvatarImage src="https://github.com/shadcn.png" />
+                            <AvatarFallback className="bg-yellow-500 text-yellow-900 text-sm font-bold">
+                                {user.name[0]}
+                            </AvatarFallback>
+                        </Avatar>
+                        <div className="flex flex-col ">
+                            <span className="text-base font-medium text-gray-400">
+                                {user.name}
+                            </span>
+                            <span className="text-sm text-gray-500" >{user.email}</span>
+                        </div>
+                    </div>
+                </DropdownMenuLabel>
+                <DropdownMenuSeparator className="bg-gray-600" />
             </DropdownMenuContent>
-        </DropdownMenu>
+        </DropdownMenu >
     )
 }
 
