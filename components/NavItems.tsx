3f90c52c5a31c31b0da363f7848e77b5d71fca44
federico <fedequinti98@gmail.com>
2026-02-25 11:37:52 +0100

    Implement home page with TradinvView widgets

--- components/NavItems.tsx
+++ components/NavItems.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { NAV_TIMES } from '@/lib/constants';
+import { NAV_ITEMS } from "@/lib/constants";
 import Link from "next/link";
 import { usePathname } from 'next/navigation';
 
@@ -14,7 +14,7 @@ const NavItems = () => {
 
     return (
         <ul className='flex flex-col sm:flex-row p-2 gap-3 sm:gap-10 font-medium'>
-            {NAV_TIMES.map(({ href, label }) => (
+            {NAV_ITEMS.map(({ href, label }) => (
                 <li key={href}>
                     <Link href={href} className={`hover:text-yellow-500 transition-colors ${isActive(href) ? 'text-gray-100' : ''
                         }`}>
@@ -22,7 +22,7 @@ const NavItems = () => {
                     </Link>
                 </li>
             ))}
-        </ul> 
+        </ul>
     )
 }
 
--- components/UserDropdown.tsx
+++ components/UserDropdown.tsx
@@ -60,7 +60,7 @@ const UserDropdown = () => {
                 <DropdownMenuSeparator className="bg-gray-600" />
                 <DropdownMenuItem onClick={handleSignOut} className="text-gray-100 text-md font-medium focus:bg-transparent focus:text-yellow-500 transition-colors cursor-pointer">
                     <LogOut className="h-4 w-4 mr-2 hidden sm:block" />
-                    LogOut
+                    Log Out
                 </DropdownMenuItem>
                 <DropdownMenuSeparator className="hidden sm:block bg-gray-600" />
                 <nav className="sm:hidden">
